1956cf18db2ddb5aeed28c18d6664c6952a10249
mgt <mgt@oi-wiki.org>
2024-08-16 00:45:39 +0800

    feat: add better drawing and clear button

--- src/VTetrisField.vue
+++ src/VTetrisField.vue
@@ -1,13 +1,14 @@
 <template>
   <div class="field-div">
-    <canvas class="vt-field" ref="canvas" @click="edit_field" />
-    <button
-      v-if="display_copy"
-      type="button"
-      class="copy-btn"
-      @click="to_tech"
-      title="copy as techmino field"
-    >{{ button_name }}</button>
+    <canvas class="vt-field" ref="canvas" @mousedown="handleMouseDown" @mousemove="handleMouseMove"
+      @mouseup="handleMouseUp" @mouseleave="handleMouseLeave" />
+    <div class="button-container">
+      <button v-if="display_copy" type="button" class="action-btn copy-btn" @click="to_tech"
+        title="copy as techmino field">{{
+          button_name }}</button>
+      <button v-if="allow_edit" type="button" class="action-btn clear-btn" @click="clearField"
+        title="clear field">Clear</button>
+    </div>
   </div>
 </template>
 
@@ -122,23 +123,13 @@ const binary_field = computed(() => {
   return res
 })
 
-function edit_field(p: any) {
-  if (!props.allow_edit) {
-    return
-  }
-  if (!canvas.value) {
-    return
-  }
-  const real_cell_size = canvas.value.offsetHeight / props.height
-  const realX = Math.floor(p.offsetX / real_cell_size)
-  const realY = props.height - 1 - Math.floor(p.offsetY / real_cell_size)
-  const current = page.value.field.at(realX, realY)
+function clearField() {
   const new_page: Page = Object.assign(Object.create(Object.getPrototypeOf(page.value)), page.value)
   let new_field = new_page.field.copy()
-  if (current === '_') {
-    new_field.set(realX, realY, "X")
-  } else {
-    new_field.set(realX, realY, "_")
+  for (let y = 0; y < props.height; y++) {
+    for (let x = 0; x < 10; x++) {
+      new_field.set(x, y, "_")
+    }
   }
   new_page.field = new_field
   page.value = new_page
@@ -146,6 +137,60 @@ function edit_field(p: any) {
     emit('field_change', binary_field.value)
   }
 }
+
+
+const isDrawing = ref(false)
+const lastCell = ref({ x: -1, y: -1 })
+const drawMode = ref('')
+
+function handleMouseDown(event: MouseEvent) {
+  if (!props.allow_edit) return
+  isDrawing.value = true
+  const { realX, realY } = getMousePosition(event)
+  const currentValue = page.value.field.at(realX, realY)
+  drawMode.value = currentValue === '_' ? 'X' : '_'
+  setCell(realX, realY, drawMode.value)
+  lastCell.value = { x: realX, y: realY }
+}
+
+function handleMouseMove(event: MouseEvent) {
+  if (!props.allow_edit || !isDrawing.value) return
+  const { realX, realY } = getMousePosition(event)
+  if (realX !== lastCell.value.x || realY !== lastCell.value.y) {
+    setCell(realX, realY, drawMode.value)
+    lastCell.value = { x: realX, y: realY }
+  }
+}
+
+function handleMouseUp() {
+  isDrawing.value = false
+}
+
+function handleMouseLeave() {
+  isDrawing.value = false
+}
+
+function getMousePosition(event: MouseEvent) {
+  if (!canvas.value) return { realX: -1, realY: -1 }
+  const rect = canvas.value.getBoundingClientRect()
+  const scaleX = canvas.value.width / rect.width
+  const scaleY = canvas.value.height / rect.height
+  const realX = Math.floor((event.clientX - rect.left) * scaleX / props.cell_size)
+  const realY = props.height - 1 - Math.floor((event.clientY - rect.top) * scaleY / props.cell_size)
+  return { realX, realY }
+}
+
+function setCell(x: number, y: number, value: string) {
+  const new_page: Page = Object.assign(Object.create(Object.getPrototypeOf(page.value)), page.value)
+  let new_field = new_page.field.copy()
+  new_field.set(x, y, value)
+  new_page.field = new_field
+  page.value = new_page
+  if (props.allow_edit) {
+    emit('field_change', binary_field.value)
+  }
+}
+
 onMounted(() => {
   if (!canvas.value) return
   canvas.value.width = 10 * props.cell_size
@@ -175,29 +220,43 @@ watch(page, () => {
 .vt-field {
   image-rendering: pixelated;
 }
-.copy-btn {
+.button-container {
+  position: absolute;
+  top: 0;
+  right: 4px;
+  display: flex;
+  flex-direction: row;
+  gap: 4px;
+}
+
+.action-btn {
   padding: 1.6px 8px;
   color: black;
   background-color: rgb(218, 218, 231);
-  border-color: #f3f3ed;
-  position: absolute;
-  top: 0px;
-  z-index: 99;
-  right: 4px;
+  border: 0 solid;
   border-radius: 0px 0px 4px 4px;
   font-size: 12px;
   font-family: ui-monospace, SFMono-Regular, Menlo, Monaco, Consolas,
     "Liberation Mono", "Courier New", monospace;
-  border: 0 solid;
   cursor: pointer;
 }
-.copy-btn:hover {
+
+.action-btn:hover {
   background-color: rgb(163, 163, 172);
+}
+
+.copy-btn {
+  z-index: 99;
+}
+
+.clear-btn {
+  z-index: 98;
 }
 .field-div {
   isolation: isolate;
   position: relative;
 }
+
 .vt-field {
   background-color: #f3f3ed;
   border-radius: 4px;
